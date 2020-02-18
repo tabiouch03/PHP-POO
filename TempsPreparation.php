e3a03dcc3cdb4bcd7c42657c5f344d5b4ba72a42
fabio guerreiro <guerreiro.fabio@outlook.fr>
2020-02-18 16:49:48 +0100

    add TempsPreparation

--- TempsPreparation.php
+++ TempsPreparation.php
@@ -1,17 +1,72 @@
 <?php 
 
-class tempsPreparation {
+class TempsPreparation {
     
     
-    private $tempscuisson;
-    private $tempsconservation;
-    private $tempstotal;
+    private $tempsCuisson;
+    private $tempsConservation;
+    private $tempsTotal;
 
-    /* SETTERS */
+    public function __construct($tempsCuisson, $tempsConservation, $tempsTotal) {
+
+        $this->setTempsCuisson($tempsCuisson);
+        $this->setTempsConservation($tempsConservation);
+        $this->setTempsTotal($tempsTotal);
+
+    }
+
+    /* GUETTERS */
 
     /*
-    *
-    *
+    * return $tempscuisson
+    * type: string
     */
-    public function 
+    public function getTempsCuisson() {
+        return $this->tempscuisson;
+    }
+
+    /*
+    * return $tempsconservation
+    * type: string
+    */
+    public function getTempsConservation() {
+        return $this->tempsconservation;
+    }
+
+    /*
+    * return $tempstotal
+    * type: string
+    */
+    public function getTempsTotal() {
+        return $this->tempstotal;
+    }
+
+    //-----------//
+    /*- SETTERS -*/
+    //----------//
+
+    /*
+    * set value tempscuisson
+    * type: string
+    */
+    public function setTempscuisson($tempscuisson) {
+        $this->tempscuisson = $tempscuisson;
+    }
+
+    /*
+    * set value tempsconservation
+    * type: string
+    */
+    public function setTempsConservation($tempsconservation) {
+        $this->tempsconservation = $tempsconservation;
+    }
+
+    /*
+    * set value tempstotal
+    * type: string
+    */
+    public function setTempsTotal($tempstotal) {
+        $this->tempstotal = $tempstotal;
+
+    }
 }
