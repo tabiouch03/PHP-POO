48c1940efdc5850c42bceea208d856a1a93d5dc7
fabio guerreiro <guerreiro.fabio@outlook.fr>
2020-02-18 15:52:09 +0100

    toBranch

---
+++ TempsPreparation.php
@@ -0,0 +1,17 @@
+<?php 
+
+class tempsPreparation {
+    
+    
+    private $tempscuisson;
+    private $tempsconservation;
+    private $tempstotal;
+
+    /* SETTERS */
+
+    /*
+    *
+    *
+    */
+    public function 
+}
